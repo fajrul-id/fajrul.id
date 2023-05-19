78b7956405c503bbc250702ff252ca576233f6c9
admin300400 <42280321+admin300400@users.noreply.github.com>
2023-05-19 10:07:54 +0700

    Update ProfileImage.tsx

--- src/components/Svg/ProfileImage.tsx
+++ src/components/Svg/ProfileImage.tsx
@@ -8,7 +8,7 @@ const ProfileImage = () => {
         <img
           src="assets/img/profile.png"
           alt="Icon"
-          className="-translate-y-9"
+          className="-translate-y-11"
         />
       </div>
     </div>
