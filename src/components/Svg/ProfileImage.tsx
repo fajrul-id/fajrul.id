d97a86ca58a923613b510348a54dfe0893e7bb16
admin300400 <42280321+admin300400@users.noreply.github.com>
2023-05-19 09:58:40 +0700

    Update ProfileImage.tsx
    Fix img profile

--- src/components/Svg/ProfileImage.tsx
+++ src/components/Svg/ProfileImage.tsx
@@ -6,7 +6,7 @@ const ProfileImage = () => {
     <div className="w-full flex items-center justify-center md:justify-end animate__animated animate__fadeInRight">
       <div className="bg rounded-full w-56 h-56 overflow-hidden">
         <img
-          src="assets/img/profile.jpg"
+          src="assets/img/profile.png"
           alt="Icon"
           className="translate-y-2"
         />
